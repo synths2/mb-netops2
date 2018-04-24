c6ee8f613654791fdcfd27691b2fcf9936e9003e
synths2 <30595482+synths2@users.noreply.github.com>
2018-04-24 15:47:47 +0100

    Add files via upload

---
+++ settings_EXAMPLE.py
@@ -0,0 +1,9 @@
+# rename this file to 'settings.py'
+
+BOT_ACCESS_TOKEN = 'Get this from Slack'
+VERIFICATION_TOKEN = 'This is also from Slack'
+API_KEY = "Meraki API key"
+DATASTORE_MAX_DAYS = 1
+CLIENT_TIMESPAN = 86400  # number of seconds to look back at client connectivity.
+BLOCK_MESSAGE = 'Security has requested this device be blocked due to suspected malware. ' \
+                'Please contact your local service desk quoting reference number: '
